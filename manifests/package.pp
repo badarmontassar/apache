5725efc5e32721a1e1d3b43993fcbcf3dcab4e0d
Badar MONTASSAR <badar.montassar@gmail.com>
2018-05-08 10:13:38 +0000

    adding apache files

---
+++ manifests/package.pp
@@ -0,0 +1,7 @@
+class apache::package (
+        String $packagename = lookup({"name => "apache.package", "default_value" => "httpd"})
+       ) { package { 'apache-package':
+                   name => $packagename,
+                   ensure => present
+           }
+}
